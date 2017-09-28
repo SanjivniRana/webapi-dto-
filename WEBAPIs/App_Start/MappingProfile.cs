907543109320004be2b3ac92f7cbcb5feacff3e9
SanjivniRana <sanjivnirana@gmail.com>
2017-09-28 11:15:00 +0530

    abc

---
+++ WEBAPIs/App_Start/MappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WEBAPIs.Dto;
+using WEBAPIs.Models;
+
+namespace WEBAPIs.App_Start
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Emp, EmpDto>();
+            CreateMap<EmpDto, Emp>();
+        }
+    }
+}
---
+++ WEBAPIs/Models/EmpContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WEBAPIs.Models
+{
+    public class EmpContext : DbContext
+    {
+        public EmpContext() : base("EmpContext") { }
+        public DbSet<Emp> Emps { get; set; }
+    }
+}
